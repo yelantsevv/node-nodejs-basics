273e96c7d7f3d723fb20d23e5fe919c7fd029f28
yelantsevv <yelantsevv@gmail.com>
2024-10-03 18:48:28 +0300

    zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { promises } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToCompress = join(__dirname, "files", "fileToCompress.txt");
+const archive = join(__dirname, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+  await promises.pipeline(
+    createReadStream(fileToCompress),
+    createGzip(),
+    createWriteStream(archive)
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
-const decompress = async () => {
-    // Write your code here 
+import { promises } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from "zlib";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToCompress = join(__dirname, "files", "fileToCompress.txt");
+const archive = join(__dirname, "files", "archive.gz");
+
+const compress = async () => {
+  await promises.pipeline(
+    createReadStream(archive),
+    createUnzip(),
+    createWriteStream(fileToCompress)
+  );
 };
 
-await decompress();
+await compress();
