6bf18f2a18cdfc5ec6e4401d01c1827910b70353
yelantsevv <yelantsevv@gmail.com>
2024-10-03 18:48:14 +0300

    wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const worker = join(__dirname, "./worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+  const CPUS = cpus().length;
+  const workers = [];
+  const results = [];
+
+  for (let i = 0; i < CPUS; i++) {
+    const n = 10 + i;
+    workers[i] = new Worker(worker, { workerData: { n } });
+
+    workers[i].on("message", (result) => {
+      results[i] = { status: "resolved", data: result };
+    });
+    workers[i].on("error", () => {
+      results[i] = { status: "error", data: null };
+    });
+  }
+  await Promise.all(
+    workers.map(
+      (worker) =>
+        new Promise((resolve) => {
+          worker.on("exit", resolve);
+        })
+    )
+  );
+
+  console.log(results);
 };
 
 await performCalculations();
