36b2363e9f8768cd69098c5f8436f6c7b291d2ac
yelantsevv <yelantsevv@gmail.com>
2024-10-03 15:55:35 +0300

    cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const res = [];
+  const args = process.argv.slice(2);
+  args.forEach((arg, index) => {
+    if (arg.startsWith("--")) {
+      res.push(`${arg.slice(2)} is ${args[index + 1]}`);
+    }
+  });
+  console.log(res.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const arrRss = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`);
+
+  console.log(arrRss.join("; "));
 };
 
 parseEnv();
