9d7c465fef359afe7349bc61185366c5cd82e9a5
yelantsevv <yelantsevv@gmail.com>
2024-10-03 15:55:58 +0300

    hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { createHash } from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const stream = createReadStream(filePath);
+
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+
+  stream.on("error", (err) => {
+    console.error("Error reading file:", err);
+  });
 };
 
 await calculateHash();
